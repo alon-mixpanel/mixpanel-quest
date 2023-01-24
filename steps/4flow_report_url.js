6501e445d0f5c877c6420e4d98d3e2da2d3b11ff
Alon Carmel <alon.carmel@gmail.com>
2023-01-24 16:22:01 +0200

    Update 4flow_report_url.js

--- steps/4flow_report_url.js
+++ steps/4flow_report_url.js
@@ -1,4 +1,4 @@
-id: send_track_event
+id: 4flow_report_url
 learningObjectives: 
 - Reproducing a bug locally 
 hints:
@@ -11,11 +11,11 @@ startFlow:
     params:
       person: keen
       messages:
-      - text: "Now that we know which users are affected, go ahead and fix the bug!"
+      - text: "So know that we know Harry is the culprit in this catastophe, we need to understand what is going on and why arent they moving past the page."
         delay: 1300
-      - text: "Only, you know, don’t—you might break something"
+      - text: "I think that Mixpanel as a way of seeing where users flow continues to."
         delay: 1400
-      - text: "It’s not that I don’t trust you, but the management definitely doesn’t, so please begin by **reproducing the bug locally** and sending me the **URL of the path** where you saw the issue. Then we’ll try to fix it on your machine, and only if it works, we’ll fix it in production."
+      - text: "Can you send me a flow report so we see where users are dropping to?"
         delay: 3500
 trigger:
   type: user_message
@@ -27,14 +27,14 @@ trigger:
       - conditionId: text_match_regex
         params:
           text: "${userMessageText}"
-          regex: ".*localhost.*@"
+          regex: ".*284637/app/flows.*@"
       then:
         do:
         - actionId: bot_message
           params:
             person: keen
             messages:
-            - text: "Wow, that is indeed a problematic screen! Looks like we forgot about mobile devices when we created this screen."
+            - text: "Great, now we can see the entire flow and the drop exactly."
               delay: 1500
         - actionId: finish_step
       else:
@@ -43,5 +43,5 @@ trigger:
           params:
             person: keen
             messages:
-            - text: "Umm, that doesn't look right. Maybe it's a different URL?"
+            - text: "Umm, that doesn't look right and a flow report. Maybe it's a different URL?"
               delay: 1400
