ba219154e48190cf25238606552376ffbcfb92c3
Alon Carmel <alon.carmel@gmail.com>
2023-01-24 16:19:32 +0200

    Update 3item_drop_name.js

--- steps/3item_drop_name.js
+++ steps/3item_drop_name.js
@@ -1,6 +1,6 @@
-id: send_track_event
+id: 3item_drop_name
 learningObjectives: 
-- Reproducing a bug locally 
+- Find the exact culprit in the data and analyze the root cause
 hints:
 - All modern web browsers can show you what your website looks like on mobile, so
   you'll be able to easily debug and develop for mobile. You can use the developer
@@ -11,11 +11,11 @@ startFlow:
     params:
       person: keen
       messages:
-      - text: "Now that we know which users are affected, go ahead and fix the bug!"
+      - text: "Ok now that we understand that theres a drop and the frequency of the drop,"
         delay: 1300
-      - text: "Only, you know, don’t—you might break something"
+      - text: "Do you think that theres a reason or a specific item that is causing this?"
         delay: 1400
-      - text: "It’s not that I don’t trust you, but the management definitely doesn’t, so please begin by **reproducing the bug locally** and sending me the **URL of the path** where you saw the issue. Then we’ll try to fix it on your machine, and only if it works, we’ll fix it in production."
+      - text: "I'm looking at the data and I have a hunch that theres a specific campaign and item that we published that is causing the issue. Can you dig around and see if you can find the specific product item name?"
         delay: 3500
 trigger:
   type: user_message
@@ -27,14 +27,14 @@ trigger:
       - conditionId: text_match_regex
         params:
           text: "${userMessageText}"
-          regex: ".*localhost.*@"
+          regex: ".*Harry potter DVD.*@"
       then:
         do:
         - actionId: bot_message
           params:
             person: keen
             messages:
-            - text: "Wow, that is indeed a problematic screen! Looks like we forgot about mobile devices when we created this screen."
+            - text: "Damn that harry! I knew that you can never trust a Gryffindor!"
               delay: 1500
         - actionId: finish_step
       else:
@@ -43,5 +43,5 @@ trigger:
           params:
             person: keen
             messages:
-            - text: "Umm, that doesn't look right. Maybe it's a different URL?"
+            - text: "Umm, that doesn't look right. Maybe different item name?"
               delay: 1400
