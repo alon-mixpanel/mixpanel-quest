859933957378d746b502b4c43e701fefe1b36fbc
Alon Carmel <alon.carmel@gmail.com>
2023-01-24 14:36:55 +0200

    Update 1create_funnel_report.js

--- steps/1create_funnel_report.js
+++ steps/1create_funnel_report.js
@@ -1,6 +1,6 @@
-id: send_track_event
+id: 1create_funnel_report
 learningObjectives: 
-- Reproducing a bug locally 
+- Learn to create a funnel report on mixpanel and sharing a url
 hints:
 - All modern web browsers can show you what your website looks like on mobile, so
   you'll be able to easily debug and develop for mobile. You can use the developer
@@ -11,11 +11,11 @@ startFlow:
     params:
       person: keen
       messages:
-      - text: "Now that we know which users are affected, go ahead and fix the bug!"
+      - text: "Hey! Whats up! ok look, we need to dig into some data real quick"
         delay: 1300
-      - text: "Only, you know, don’t—you might break something"
+      - text: "We've been using Mixpanel for quite some time, https://mixpanel.com/project/2131808/view/284637/app/insights#GzpTEHqSjp4b this is our Mixpanel."
         delay: 1400
-      - text: "It’s not that I don’t trust you, but the management definitely doesn’t, so please begin by **reproducing the bug locally** and sending me the **URL of the path** where you saw the issue. Then we’ll try to fix it on your machine, and only if it works, we’ll fix it in production."
+      - text: "I need you to create a funnel report for me to look at that includes a funnel of new details page > add to cart > complete purchase. Once you are ready send me the URL so I can look at it."
         delay: 3500
 trigger:
   type: user_message
@@ -27,14 +27,14 @@ trigger:
       - conditionId: text_match_regex
         params:
           text: "${userMessageText}"
-          regex: ".*localhost.*@"
+          regex: ".*284637/app/funnels.*@"
       then:
         do:
         - actionId: bot_message
           params:
             person: keen
             messages:
-            - text: "Wow, that is indeed a problematic screen! Looks like we forgot about mobile devices when we created this screen."
+            - text: "Thats great, looks like the funnel report is ok, make sure you have all the events I asked for in order. We need to dig into it more."
               delay: 1500
         - actionId: finish_step
       else:
